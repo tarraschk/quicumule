849171b2cce00d08fd35e8cc8628574771bd3107
JA <jeanlain.re@gmail.com>
2013-02-23 18:58:28 +0100

    quicumule : - traitement des données

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-$(function(){
-    
-})
+function getCumualards(departement_num){s
+    $.when($.getJSON('datas/deputes.json'), $.getJSON('datas/senateurs.json'), $.getJSON('datas/ministres_tries.json')).done(function(data1, data2, data3) {
+        //Quand les 3 requêtes sont terminés
+        var cumulards = [];
+        var deputes = data1[0].values;
+
+        $.each(deputes, function(key, val) {
+            if(val.departement_num == departement_num){
+                cumulards.push(val);
+            }
+        });
+        
+        var senateurs = data2[0].values;
+
+        $.each(senateurs, function(key, val) {
+            if(val.departement_num == departement_num){
+                cumulards.push(val);
+            }
+        });
+        
+        var ministres = data3[0].values;
+
+        $.each(ministres, function(key, val) {
+            if(val.departement_num == departement_num){
+                cumulards.push(val);
+            }
+        });
+        console.dir(cumulards);
+    });
+}
+getCumualards('44');
 
