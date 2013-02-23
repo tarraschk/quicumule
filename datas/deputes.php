456afb1a6dc538967d108ae90cb794c1ec080f0e
JA <jeanlain.re@gmail.com>
2013-02-23 11:18:11 +0100

    quicumule : - deputes json

---
+++ datas/deputes.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Récupération de la liste des députés
+ * 
+ * >> genere deputes.json
+ * format : 
+ * 
+ * [{
+ *  nom: '',
+ *  prenom : '',
+ *  twitter : '',
+ *  facebook : '',
+ *  lieu : ''
+ * }]
+ * 
+ * 
+ */
+
+
+$src_page_data = '';
+
+
+?>
